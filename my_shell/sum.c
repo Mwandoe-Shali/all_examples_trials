af2c419646013a12a5e68d47d428d0d1f9660b5c
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-08-10 07:33:18 -0700

    PID trial 2

---
+++ my_shell/sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main ()
+{
+    int i, j, k;
+    i = 45;
+    j = 56;
+    k = 43;
+    int sum = (i + j + k);
+
+    pid_t pid;
+
+    printf("The sum of the numbers is %d\n", sum);
+
+    pid = getpid();
+    printf("The pid is: %u\n", pid);
+
+    return (0);
+
+}
+
