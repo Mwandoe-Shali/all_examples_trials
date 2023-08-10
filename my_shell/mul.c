db46717d226143884db778f0677645fcd3db37a6
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-08-10 07:24:14 -0700

    trial 1 PID

---
+++ my_shell/mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int i = 5;
+    int j = 4;
+    int mul = (i * j);
+
+    pid_t ppid;
+    
+    printf("The result of multiplication is %d\n", mul);
+
+    ppid = getppid();
+    printf("The ppid for this process is %u\n", ppid);
+}
+
