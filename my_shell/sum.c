e5f23e15f56c16ef7de407004e5c6d98ac93cd21
Mwandoe-Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-08-12 16:13:29 +0300

    mixed decarations for pid

--- my_shell/sum.c
+++ my_shell/sum.c
@@ -4,14 +4,13 @@
 int main ()
 {
     int i, j, k, sum;
+    pid_t pid;
     i = 45;
     j = 56;
     k = 43;
     
     sum = (i + j + k);
-
-    pid_t pid;
-
+    
     printf("The sum of the numbers is %d\n", sum);
 
     pid = getpid();
