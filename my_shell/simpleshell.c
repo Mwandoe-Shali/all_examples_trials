d3c05d9457f6c34212a8e7f66ecde37108e6cd3b
Mwandoe-Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-08-12 16:22:17 +0300

    Update simpleshell.c

--- my_shell/simpleshell.c
+++ my_shell/simpleshell.c
@@ -29,7 +29,7 @@ line[nread - 1] = '\0'; /* Remove newline character */
 child_pid = fork(); /* create child process */
 if (child_pid == 0)
 {
-char *args[] = {(line = NULL), NULL}; /* Child process */
+char *args[] = {line = NULL, NULL}; /* Child process */
 execve(line, args, NULL);
 perror(line); /* Print error if execve fails */
 exit(1);
