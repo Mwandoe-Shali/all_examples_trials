04aa1f631f2866b1da13baff39603f9508054a67
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-08-16 00:48:44 +0300

    included helper functions.

--- my_shell/shell.h
+++ my_shell/shell.h
@@ -12,6 +12,11 @@
 #include <string.h>
 #include <signal.h>
 
+/*-----helper functions------*/
+size_t _strlen(const char *str);
+char *_strcpy(char *dest, const char *src);
+void *_memcpy(void *dest, const void *src, size_t n);
+char *_strdup(const char *str);
 
 #endif /* SHELL_H */
 
