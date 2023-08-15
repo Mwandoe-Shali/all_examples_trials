5d1f35c0502572ef13961cd9d07ab2c181fb06db
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-08-16 00:56:27 +0300

    Update helper_functions.c for strlen description.

--- my_shell/helper_functions.c
+++ my_shell/helper_functions.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - Returns the length of a string
- * @s: Pointer to the string
+ * @str: Pointer to the string
  * Return: Length of the string
  */
 size_t _strlen(const char *str)
