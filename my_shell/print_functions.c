e0d4b52d86be2196e682b4749186a017b2d3db91
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-08-16 15:35:08 +0300

    included _printf

--- my_shell/print_functions.c
+++ my_shell/print_functions.c
@@ -33,3 +33,53 @@ int _puts(const char *str)
 	/* Return success */
 	return (0);
 }
+
+/**
+ * _printf - A custom implementation of the printf function
+ *
+ * @format: A format string that specifies the output
+ * @...: The arguments to be printed
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format); /* Initialize va_list */
+	/* Loop through the format string */
+	while (*format)
+	{
+		/* Check for a format specifier */
+		if (*format == '%')
+			/* Get the next character in the format string */
+			format++;
+
+		/* Switch on the format specifier */
+		switch (*format)
+		{
+			case 'c':
+				count += _putchar(va_arg(args, int)); /* character */
+				break;
+			case 's':
+				count += _puts(va_arg(args, const char *)); /* string */
+				break;
+			default:
+				/* Unknown format specifier */
+				count += _putchar('%');
+				count += _putchar(*format);
+				break;
+		}
+	}
+	else
+	{
+		/* Print the character */
+		count += _putchar(*format);
+	}
+
+	format++; /* Next character */
+
+	va_end(args); /* Clean up the va_list */
+	return (count); /* Return num of chars printed */
+}
